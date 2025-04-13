81a8b8f08550eaef6736f5e7ed15e787bd4a9e22
gabrielo <gabrielorteganicolas@gmail.com>
2025-04-13 20:11:11 +0200

    implemented opening cells logic

--- src/game/bomb.rs
+++ src/game/bomb.rs
@@ -5,8 +5,8 @@ use std::collections::HashSet;
 use rand::seq::SliceRandom;
 use rand::rng;
 
-pub fn generate_bombs(game: &mut Game, start_position: Position, num_bombs: usize) {
-    let forbidden_positions = get_adjacent_positions(start_position, game.width, game.height);
+pub fn generate_bombs(game: &mut Game) {
+    let forbidden_positions = get_adjacent_positions(game.player_position, game.width, game.height);
     let all_positions = get_all_positions(game.width, game.height);
     let forbidden_set: HashSet<Position> = forbidden_positions.iter().cloned().collect();
     let mut valid_positions: Vec<Position> = all_positions
@@ -15,10 +15,22 @@ pub fn generate_bombs(game: &mut Game, start_position: Position, num_bombs: usiz
         .collect();
     let mut rng = rng();
     valid_positions.shuffle(&mut rng);
+    let total_squares = game.height * game.width;
+    let num_bombs = (total_squares as f64 * 0.15).round() as usize; 
     for i in 0..num_bombs {
         let x = valid_positions[i].x as usize;
         let y = valid_positions[i].y as usize;
         game.board[y][x].is_bomb = true;
+        increase_adjacency_count_of_surrounding_cells(game, game.player_position);
     }
     game.bombs_placed = true;
 }
+
+fn increase_adjacency_count_of_surrounding_cells(game: &mut Game, position: Position) {
+    let adjacent_positions = get_adjacent_positions(position, game.width, game.height);
+    for position in adjacent_positions.iter(){
+        let adjacent_x: usize = position.x.try_into().expect("position x too large");
+        let adjacent_y: usize = position.y.try_into().expect("position y too large");
+        game.board[adjacent_x][adjacent_y].adjacent_bombs += 1;
+    }
+} 
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,11 +1,13 @@
 mod cell;
 mod position;
-pub mod bomb;
-pub mod helpers;
+mod bomb;
+mod helpers;
 
 use cell::Cell;
 use position::Position;
 use std::convert::TryInto;
+use recursive::recursive;
+use helpers::get_adjacent_positions;
 
 pub struct Game {
     pub width: usize,
@@ -14,6 +16,7 @@ pub struct Game {
     pub board: Vec<Vec<Cell>>,
     pub player_position: Position,
     pub bombs_placed: bool,
+    pub game_is_active: bool,
 }
 
 impl Game {
@@ -34,6 +37,55 @@ impl Game {
             board,
             player_position: initial_position,
             bombs_placed: false,
+            game_is_active: true,
         }
     }
+
+    pub fn generate_bombs(&mut self) {
+        bomb::generate_bombs(self);
+    }
+
+    pub fn open_current_cell(&mut self) {
+        if !self.game_is_active {
+            return;
+        }
+        if !self.bombs_placed {
+            self.generate_bombs();
+        }
+        let x: usize = self.player_position.x.try_into().expect("position x too large");
+        let y: usize = self.player_position.y.try_into().expect("position y too large");
+        if self.board[x][y].is_open {
+            return;
+        }
+        if self.board[x][y].is_bomb {
+            // Game ends
+            self.game_is_active = false;
+            return;
+        }
+        self.board[x][y].is_open = true;
+        Self::open_adjacent_cells(&mut self.board, self.player_position, self.width, self.height);
+    }
+
+    #[recursive]
+    fn open_adjacent_cells(board: &mut Vec<Vec<Cell>>, position: Position, width: usize, height: usize) {
+        let adjacent_positions = get_adjacent_positions(position, width, height);
+        for position in adjacent_positions.iter() {
+            let adjacent_x: usize = position.x.try_into().expect("position x too large");
+            let adjacent_y: usize = position.y.try_into().expect("position y too large");
+            // This check should not be necessary, but just in case
+            if board[adjacent_x][adjacent_y].is_bomb {
+                continue;
+            }
+            // Cells with adjacent bombs are opened
+            if board[adjacent_x][adjacent_y].adjacent_bombs != 0 {
+                board[adjacent_x][adjacent_y].is_open = true;
+                continue;
+            }
+            // If cell is empty, open it and its adjacents
+            board[adjacent_x][adjacent_y].is_open = true;
+            Self::open_adjacent_cells(board, *position, width, height);
+        }
+    }
+
+    pub fn flag_current_cell() {} 
 }
